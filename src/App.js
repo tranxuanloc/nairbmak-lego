f6a7ab1be9e418e3ce78ca96738c4d4b841f97d7
Phong <phongvh50ca@gmail.com>
2018-06-12 10:43:54 +0700

    Add lego utility tool & lego logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import lego from './lego.png';
 import './static/css/index.css';
 
 class App extends Component {
@@ -7,27 +8,34 @@ class App extends Component {
     return (
       <div id="site_wrapper">
         <header className="site-header">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="navbar-brand mr-auto"><a href="/"><img src={logo} className="Kambria-logo" alt="logo" /></a></div>
-                    <nav className="navbar">
-                        <ul className="nav navbar-nav">
-                            <li><a aria-current="false" href="/">Kambria.io</a></li>
-                        </ul>
-                    </nav>
-                </div>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="navbar-brand mr-auto"><a href="/"><img src={logo} className="kambria-logo" alt="kambria-logo" /></a></div>
+              <nav className="navbar">
+                <ul className="nav navbar-nav">
+                  <li><a aria-current="false" href="/">Kambria.io</a></li>
+                  <li><a aria-current="false" href="/">How to use</a></li>
+                  <li><a aria-current="false" href="/">Components</a></li>
+                </ul>
+              </nav>
             </div>
+          </div>
         </header>
         <div className="main">
           <div className="container">
-              <h1>Welcome to Kambria!</h1>
-            </div>
+            <img src={lego} className="lego-logo" alt="Lego logo" />
+            <hr />
+            <h1>Welcome to Kambria Lego!</h1>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
         </div>
         <footer className="footer">
-            <div className="container d-flex flex-row flex-wrap align-items-center justify-content-center">
-                <div className="item copyright">Copyright © 2018 Kambria.io</div>
-                <div className="item contact"><a href="mailto:info@kambria.io" className="color-gray color-base-hover">info@kambria.io</a></div>
-            </div>
+          <div className="container d-flex flex-row flex-wrap align-items-center justify-content-center">
+            <div className="item copyright">Copyright © 2018 Kambria.io</div>
+            <div className="item contact"><a href="mailto:info@kambria.io" className="color-gray color-base-hover">info@kambria.io</a></div>
+          </div>
         </footer>
       </div>
     );
