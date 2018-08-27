5b425d515d3fdcf0db77691a895f5254185c45d9
Tung Pham <tung.42@gmail.com>
2018-08-27 10:43:33 +0700

    Loading test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import Home from 'views/pages/Home';
 import KambriaComponents from 'views/pages/KambriaComponents';
 import BootstrapComponents from 'views/pages/BootstrapComponents';
 import Redux from 'views/pages/Redux';
+import Loading from 'views/pages/Loading';
 
 export default [
   { path: "/", component: Home, type: 'public' },
   { path: "/kambria-components", component: KambriaComponents, type: 'private' },
   { path: "/bootstrap-components", component: BootstrapComponents, type: 'private' },
-  { path: "/redux", component: Redux, type: 'private' }
+  { path: "/redux", component: Redux, type: 'private' },
+  { path: "/loading", component: Loading, type: 'public' }
 ];
---
+++ src/views/pages/Loading.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { css } from 'react-emotion';
+// First way to import
+import { PropagateLoader } from 'react-spinners';
+ 
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
+ 
+class LoadingComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+  render() {
+    return (
+      <div className='page-loading'>
+        <PropagateLoader
+          className={override}
+          sizeUnit={"px"}
+          size={15}
+          color={'#1AEEC2'}
+          loading={this.state.loading}
+        />
+      </div>; 
+    )
+  }
+}
+export default LoadingComponent;
